b55d2cf9ef45037ebc6b7c2c2e7e1fe936f6f516
remembe-past <q086536@gmail.com>
2024-06-08 21:39:59 +0800

    init

--- src/function_Run_ImputeHiFI.py
+++ src/function_Run_ImputeHiFI.py
@@ -169,9 +169,282 @@ def preprocess_DNA_FISH(input_data_file,data_dir):
     for one_key, one_value in zip(keys_list, values_list):
         data[one_key] = one_value
     data_pkl_path = 'data.pkl'
-    # data_dir=r"/mnt/disk1/scfan/data/Cai_21_Nature/DNA_25kb"
+
     basic.store_variable_from_pikle_file(os.path.join(data_dir, data_pkl_path),
                                          data)
+    RNA_seqFISH_dir=data_dir
+    data_path = os.path.join(RNA_seqFISH_dir, "mRNA-copy-number-E14-replicate{}.csv")
+
+    rep1_data = pd.read_csv(data_path.format(1))
+    gene_name_list = list(rep1_data.columns[7:])
+    rep1_data["cell_type"] = 'rep1'
+    rep2_data = pd.read_csv(data_path.format(2))
+    gene_name_list += list(rep2_data.columns[7:])
+    rep2_data["cell_type"] = 'rep2'
+
+    data_path = os.path.join(RNA_seqFISH_dir, "mRNA-copy-number-E14-clonal{}hr.csv")
+    clone_24_data = pd.read_csv(data_path.format(24))
+    gene_name_list += list(clone_24_data.columns[7:])
+    clone_24_data["cell_type"] = '24hr'
+    clone_48_data = pd.read_csv(data_path.format(48))
+    gene_name_list += list(clone_48_data.columns[7:])
+    clone_48_data["cell_type"] = '48hr'
+    gene_name_list = list(np.unique(gene_name_list))
+    RNA_origin_data = pd.concat([rep1_data, rep2_data, clone_24_data, clone_48_data], ignore_index=True, sort=False)
+    RNA_origin_data['cell_name'] = RNA_origin_data['fov'].astype(str) + '_3_' + RNA_origin_data['cellID'].astype(
+        str) + '_' + RNA_origin_data['cell_type'].astype(str)
+    RNA_origin_data.index = RNA_origin_data['cell_name']
+    RNA_name_array = RNA_origin_data['cell_name'].values
+    RNA_df=RNA_origin_data[gene_name_list]
+
+    RNA_df=RNA_df.fillna(0)
+    RNA_adata = sc.AnnData(RNA_df)
+    RNA_adata.obs['cell_type'] = RNA_origin_data['cell_type']
+    RNA_adata.obs['cell_name'] = RNA_origin_data['cell_name']
+    temp_path = os.path.join(data_dir, 'RNA_adata.h5ad')
+    RNA_adata.write(temp_path)
+    data_pkl_path = 'data.pkl'
+    all_cell_data=basic.load_variable_from_pikle_file(os.path.join(data_dir, data_pkl_path))
+    all_cell_name_list = all_cell_data['chrom_ids']
+    dna_zxys = all_cell_data['dna_zxys']
+    cluster_label = all_cell_data['cluster_label']
+    new_cell_name_list = []
+    new_dna_zxys = []
+    new_cluster_label = []
+    for i, one_cell_name in enumerate(all_cell_name_list):
+        if one_cell_name in RNA_name_array:
+            new_cell_name_list.append(one_cell_name)
+            new_dna_zxys.append(dna_zxys[i])
+            new_cluster_label.append(cluster_label[i])
+    new_cell_data = copy.deepcopy(all_cell_data)
+    new_cell_data['chrom_ids'] = new_cell_name_list
+    new_cell_data['dna_zxys'] = new_dna_zxys
+    new_cell_data['cluster_label'] = new_cluster_label
+    data_pkl_path = 'align_name_with_RNA_data.pkl'
+    basic.store_variable_from_pikle_file(os.path.join(r"/mnt/disk1/scfan/data/Cai_21_Nature/DNA_25kb", data_pkl_path),
+                                         new_cell_data)
+
+    data = new_cell_data
+    first_seen_probe_data_dir = data_dir
+    ImputeHiFI_data_dir = os.path.join(first_seen_probe_data_dir, 'ImputeHiFI')
+    RNA_FISH_adata=RNA_adata
+    chrom_ids_to_index_dict = {}
+    cell_name_list = []
+    for index, chrom_ids in enumerate(data['chrom_ids']):
+        chrom_ids_to_index_dict[str(chrom_ids)] = index
+        cell_name_list.append(str(chrom_ids))
+    cell_name_array = np.array(cell_name_list)
+    chrom_index_to_ids_dict = {index: chrom_id for chrom_id, index in chrom_ids_to_index_dict.items()}
+    chr_name_list = data['chrom_name'][0:20]
+    num_cells = len(chrom_ids_to_index_dict)
+    num_chromosomes = len(chr_name_list)
+
+    len_num = 60
+
+    # 初始化结构，每个cell有一个列表，列表中有num_chromosomes个元素
+    max_all_cell_dna_list = [[] for _ in range(num_cells)]
+    min_all_cell_dna_list = [[] for _ in range(num_cells)]
+    max_all_cell_dna_list_not_missing_rate = [[] for _ in range(num_cells)]
+    min_all_cell_dna_list_not_missing_rate = [[] for _ in range(num_cells)]
+    choice_list = [[] for _ in range(num_cells)]  # 新增列表用于记录选择的是0还是1
+
+    # 遍历每个cell和其对应的染色体
+    for cell_index, (one_cell_name, one_cell_ids) in enumerate(chrom_ids_to_index_dict.items()):
+        for chr_ids, chr_name in enumerate(chr_name_list):
+            # 计算非缺失率
+            not_nan_sum_0 = np.sum(~np.isnan(all_cell_dna_list[one_cell_ids][chr_ids][0])[:, 0])
+            not_nan_sum_1 = np.sum(~np.isnan(all_cell_dna_list[one_cell_ids][chr_ids][1])[:, 0])
+            not_missing_rate_0 = not_nan_sum_0 / len_num
+            not_missing_rate_1 = not_nan_sum_1 / len_num
+
+            # 根据非缺失率将数据存储到max或min列表中，并记录选择
+            if not_missing_rate_0 > not_missing_rate_1:
+                max_all_cell_dna_list[cell_index].append(all_cell_dna_list[one_cell_ids][chr_ids][0])
+                max_all_cell_dna_list_not_missing_rate[cell_index].append(not_missing_rate_0)
+                min_all_cell_dna_list[cell_index].append(all_cell_dna_list[one_cell_ids][chr_ids][1])
+                min_all_cell_dna_list_not_missing_rate[cell_index].append(not_missing_rate_1)
+                choice_list[cell_index].append(0)
+            else:
+                max_all_cell_dna_list[cell_index].append(all_cell_dna_list[one_cell_ids][chr_ids][1])
+                max_all_cell_dna_list_not_missing_rate[cell_index].append(not_missing_rate_1)
+                min_all_cell_dna_list[cell_index].append(all_cell_dna_list[one_cell_ids][chr_ids][0])
+                min_all_cell_dna_list_not_missing_rate[cell_index].append(not_missing_rate_0)
+                choice_list[cell_index].append(1)
+    all_cell_mean_not_missing_rate=[]
+    for cell_index, (one_cell_name, one_cell_ids) in enumerate(chrom_ids_to_index_dict.items()):
+        one_cell_mean_not_missing_rate=np.mean(min_all_cell_dna_list_not_missing_rate[cell_index]+max_all_cell_dna_list_not_missing_rate[cell_index])
+        all_cell_mean_not_missing_rate.append(one_cell_mean_not_missing_rate)
+
+    first_seen_probe_data = copy.deepcopy(data)
+    first_seen_probe_data['all_cell_dna_list'] = max_all_cell_dna_list
+    first_seen_probe_data['chrom_ids_to_index_dict'] = chrom_ids_to_index_dict
+
+    first_seen_probe_data['chrom_index_to_ids_dict'] = chrom_index_to_ids_dict
+    first_seen_probe_data['chr_name_list'] = chr_name_list
+    first_seen_probe_data['cell_name_array'] = cell_name_array
+    first_seen_probe_data['cell_name_list'] = cell_name_list
+    result_list = []
+
+
+    for cell_index, (one_cell_name, one_cell_ids) in enumerate(chrom_ids_to_index_dict.items()):
+        for chr_index, chr_name in enumerate(chr_name_list):
+            # 获取当前cell和染色体的DNA数据
+            dna_data = max_all_cell_dna_list[cell_index][chr_index]
+
+            # 计算总探针数量和NaN探针数量
+            total_probe_number = len(dna_data)
+            nan_probe_number = np.sum(np.isnan(dna_data)[:, 0])
+
+            # 计算缺失率
+            missing_rate = nan_probe_number / total_probe_number if total_probe_number else np.nan
+
+            # 将结果存储到列表中
+            result_list.append({
+                'cell_name': one_cell_name,
+                'chr_name': chr_name,
+                'total_probe_number': total_probe_number,
+                'nan_probe_number': nan_probe_number,
+                'missing_rate': missing_rate
+            })
+
+    df_columns = ['cell_name', 'chr_name', 'total_probe_number', 'nan_probe_number', 'missing_rate']
+    each_chr_missing_rate_df = pd.DataFrame(result_list, columns=df_columns)
+    missing_rate_df = each_chr_missing_rate_df.set_index(['cell_name', 'chr_name'])
+    first_seen_probe_data['each_chr_missing_rate_df'] = each_chr_missing_rate_df
+    first_seen_probe_data['missing_rate_df'] = missing_rate_df
+    chr_bin_length_dic = {}
+    for chr_name in chr_name_list:
+        chr_bin_length_dic[chr_name] = len_num
+    first_seen_probe_data['chr_bin_length_dic'] = chr_bin_length_dic
+    cell_number = len(first_seen_probe_data['chrom_ids'])
+    first_seen_probe_data['cell_number'] = cell_number
+    cluster_label_list = first_seen_probe_data['cluster_label']
+    cluster_label_name_dict = first_seen_probe_data['cluster_label_name']
+    result_list = []
+    for cell_index, label_idx in enumerate(cluster_label_list):
+        one_cell_name = chrom_index_to_ids_dict[cell_index]
+        cell_type = cluster_label_name_dict[label_idx]
+        result_list.append({
+            'cell_name': one_cell_name,
+            'cell_type': cell_type,
+        })
+    cell_type_df = pd.DataFrame(result_list)
+    cell_type_df.index = cell_type_df['cell_name']
+    first_seen_probe_data['cell_type_df'] = cell_type_df
+    adata_obs_df = cell_type_df
+    first_seen_probe_data['adata_obs_df'] = adata_obs_df
+    spaital_dist_suffix = 'spatial_dist'
+    prxoimtiy_score_suffix = 'proximity_score'
+    first_seen_probe_data['spaital_dist_suffix'] = spaital_dist_suffix
+    first_seen_probe_data['prxoimtiy_score_suffix'] = prxoimtiy_score_suffix
+    store_path = os.path.join(first_seen_probe_data_dir, f'first_seen_probe_data_{spaital_dist_suffix}.h5ad')
+    ImputeHiFI.generate_spatial_distance_h5ad.generate_spatial_h5ad(chr_name_list, cell_number, all_cell_dna_list,
+                                                                    chr_bin_length_dic, chrom_index_to_ids_dict,
+                                                                    adata_obs_df, store_path, )
+    store_path = os.path.join(first_seen_probe_data_dir, f'first_seen_probe_data_{spaital_dist_suffix}.h5ad')
+    DNA_FISH_spad_adata = sc.read(store_path)
+    adata = DNA_FISH_spad_adata.copy()
+    cell_types = adata.obs['cell_type'].unique()
+
+    # 创建一个字典来存储每种细胞类型的子集数据
+    cell_type_subsets = {}
+
+    for cell_type in cell_types:
+        # 使用 adata[adata.obs['cell_type'] == cell_type] 来选择特定类型的细胞
+        cell_type_subset = adata[adata.obs['cell_type'] == cell_type]
+        # 将子集数据存储在字典中
+        cell_type_subsets[cell_type] = cell_type_subset
+    p = 50
+    chr_cell_type_center_value_dict = {}
+    for chr_name in tqdm(chr_bin_length_dic.keys()):
+        num_cell_types = len(cell_types)
+        for index, cell_type in enumerate(cell_types):
+            title_name = f"{cell_type} {chr_name}"
+            bin_length = chr_bin_length_dic[chr_name]
+            Example_df = ImputeHiFI.generate_Example_bin_pair_df(chr_name, bin_length)
+            temp_array = cell_type_subsets[cell_type][:, Example_df.index].copy().X
+            title_name += f" ({temp_array.shape[0]} chromosomes)"
+            Example_df['data'] = np.nanmedian(temp_array, axis=0)
+            matrix_array = ImputeHiFI.convert_bin_pair_df_to_array(bin_length, Example_df)
+            matrix_array = matrix_array + matrix_array.T
+            np.fill_diagonal(matrix_array, 0)
+            rows, cols = np.triu_indices(matrix_array.shape[0], k=1)
+
+            # 使用这些索引提取上三角矩阵的值
+            upper_triangle_values = matrix_array[rows, cols]
+
+            # 从上三角值中过滤出非NaN的值
+            non_nan_upper_triangle_values = upper_triangle_values[~np.isnan(upper_triangle_values)]
+            #         p=50
+            center_value = np.percentile(non_nan_upper_triangle_values, p)
+            chr_cell_type_center_value_dict[chr_name + cell_type] = center_value
+    chr_cell_type_center_value_df = pd.DataFrame(list(chr_cell_type_center_value_dict.items()),
+                                                 columns=['Chr_Cell_Type', 'Center_Value'])
+    chr_cell_type_center_value_df.index = chr_cell_type_center_value_df['Chr_Cell_Type']
+    first_seen_probe_data['chr_cell_type_center_value_df'] = chr_cell_type_center_value_df
+    store_path = os.path.join(first_seen_probe_data_dir, f'first_seen_probe_data_{spaital_dist_suffix}.h5ad')
+    DNA_FISH_spad_adata = sc.read(store_path)
+
+    store_path = os.path.join(first_seen_probe_data_dir, f'first_seen_probe_data_{prxoimtiy_score_suffix}.h5ad')
+    ImputeHiFI.generate_proximity_score_h5ad.generate_proximity_h5ad(DNA_FISH_spad_adata, chr_name_list,
+                                                                     chr_cell_type_center_value_df,
+                                                                     chr_bin_length_dic, adata_obs_df, store_path,
+                                                                     kernerl_number=None)
+    chr_name_list = first_seen_probe_data['chr_name_list']
+    cell_number = first_seen_probe_data['cell_number']
+    all_cell_dna_list = first_seen_probe_data['all_cell_dna_list']
+    cell_name_array = first_seen_probe_data['cell_name_array']
+    All_cell_RMSD_dict = ImputeHiFI.generate_cell_cell_RMSD.get_All_cell_RMSD_dict(chr_name_list, cell_number,
+                                                                                   all_cell_dna_list,
+                                                                                   cell_name_array, kernerl_number=None)
+    first_seen_probe_data['All_cell_RMSD_dict'] = All_cell_RMSD_dict
+    All_cell_RNA_RMSD_dict = ImputeHiFI.generate_RNA_FISH_cell_cell_RMSD.get_RNA_FISH_cell_cell_RMSD_dict(
+        RNA_FISH_adata, chr_name_list, cell_name_array,
+        All_cell_RMSD_dict, kernerl_number=None)
+    first_seen_probe_data['All_cell_RNA_RMSD_dict'] = All_cell_RNA_RMSD_dict
+
+    cell_number = first_seen_probe_data['cell_number']
+    all_cell_dna_list = first_seen_probe_data['all_cell_dna_list']
+    chr_num = len(first_seen_probe_data['chr_name_list'])
+    Extriming_chr_list = []
+
+    for chr_idx in tqdm(range(chr_num)):
+        for cell_idx in range(cell_number):
+            _zxys = all_cell_dna_list[cell_idx][chr_idx]
+            probe_number = _zxys.shape[0]
+            nan_ids = np.where(np.isnan(all_cell_dna_list[cell_idx][chr_idx][:, 0]))[0]
+            (continue_number_list, continue_start_ids) = FillNANBasic.FillNANMean.get_NAN_continue_info(nan_ids)
+            _zxys_fill_NAN, extreaming_condition_1 = FillNANBasic.FillNANMean.fill_NAN_v1(_zxys,
+                                                                                          continue_number_list,
+                                                                                          continue_start_ids,
+                                                                                          probe_number)
+            if not extreaming_condition_1:
+                Extriming_chr_list.append(_zxys_fill_NAN)
+                break
+    Impute_dna_list = copy.deepcopy(all_cell_dna_list)
+    for cell_idx in tqdm(range(cell_number)):
+        for chr_idx in range(chr_num):
+            _zxys = all_cell_dna_list[cell_idx][chr_idx]
+            probe_number = _zxys.shape[0]
+            nan_ids = np.where(np.isnan(all_cell_dna_list[cell_idx][chr_idx][:, 0]))[0]
+
+            if (len(nan_ids) > 0):
+                (continue_number_list, continue_start_ids) = FillNANBasic.FillNANMean.get_NAN_continue_info(nan_ids)
+
+                _zxys_fill_NAN, extreaming_condition_1 = FillNANBasic.FillNANMean.fill_NAN_v1(_zxys,
+                                                                                              continue_number_list,
+                                                                                              continue_start_ids,
+                                                                                              probe_number)
+            else:
+                _zxys_fill_NAN = _zxys
+                extreaming_condition_1 = False
+            if (extreaming_condition_1):
+                _zxys_fill_NAN = Extriming_chr_list[chr_idx]
+            Impute_dna_list[cell_idx][chr_idx] = _zxys_fill_NAN
+    first_seen_probe_data['Linear_all_cell_dna_list'] = Impute_dna_list
+    first_seen_probe_data['Extriming_chr_list'] = Extriming_chr_list
+    first_seen_probe_data_path = os.path.join(first_seen_probe_data_dir, 'first_seen_probe_data.pkl')
+    basic.store_variable_from_pikle_file(first_seen_probe_data_path, first_seen_probe_data)
 def generate_Impute_result(chr_bin_length_dic, base_coef,short_range_HiC_weight_bound,
                       long_range_HiC_weight_bound,all_range_diag_sum_weight_HiC_bound,
                         short_range_diag,background_cell_num,
@@ -298,6 +571,23 @@ def run_ImputeHiFI_mode_1(data_dir,input_data,HiC_weight_bound=0.4,neighbor_cell
     DNA_FISH_name_list=cell_name_list
     DNA_FISH_name_split_list = np.array_split(DNA_FISH_name_list, kernerl_number)
     chr_cell_type_center_value_df = chr_cell_type_center_value_df.set_index('Chr_Cell_Type')
+
+    df_name_list = []
+    df_chr_list = []
+    df_number_list = []
+    for one_cell_name in cell_name_list:
+        for one_chr in chr_bin_length_dic.keys():
+            probe_num = chr_bin_length_dic[one_chr]
+            df_name_list.append(one_cell_name)
+            df_chr_list.append(one_chr)
+            df_number_list.append(probe_num)
+
+    missing_rate_df = pd.DataFrame()
+    missing_rate_df['cell_name'] = df_name_list
+    missing_rate_df['chr_name'] = df_chr_list
+    missing_rate_df['total_probe_number'] = df_number_list
+    missing_rate_df = missing_rate_df.set_index(['cell_name', 'chr_name'])
+
     import multiprocessing as mp
     num_threads = kernerl_number
     base_coef = 0.001
